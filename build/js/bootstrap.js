22bb61a7dfb3ed4761cf325dec4d9f836c335bae
Jagruk Poudel <jagruk@bitbucket.org>
2023-11-23 13:59:42 +0930

    themes/components

--- build/js/bootstrap.js
+++ build/js/bootstrap.js
@@ -1911,7 +1911,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./base-component.js":3,"./dom/event-handler.js":8,"./util/component-functions.js":12,"./util/index.js":14}],3:[function(require,module,exports){
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./util/component-functions.js":13,"./util/index.js":15}],3:[function(require,module,exports){
 /*!
   * Bootstrap base-component.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -1997,7 +1997,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./dom/data.js":7,"./dom/event-handler.js":8,"./util/config.js":13,"./util/index.js":14}],4:[function(require,module,exports){
+},{"./dom/data.js":7,"./dom/event-handler.js":8,"./util/config.js":14,"./util/index.js":15}],4:[function(require,module,exports){
 /*!
   * Bootstrap button.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -2078,7 +2078,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./base-component.js":3,"./dom/event-handler.js":8,"./util/index.js":14}],5:[function(require,module,exports){
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./util/index.js":15}],5:[function(require,module,exports){
 /*!
   * Bootstrap carousel.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -2468,7 +2468,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/manipulator.js":9,"./dom/selector-engine.js":10,"./util/index.js":14,"./util/swipe.js":15}],6:[function(require,module,exports){
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/manipulator.js":9,"./dom/selector-engine.js":10,"./util/index.js":15,"./util/swipe.js":16}],6:[function(require,module,exports){
 /*!
   * Bootstrap collapse.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -2719,7 +2719,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/selector-engine.js":10,"./util/index.js":14}],7:[function(require,module,exports){
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/selector-engine.js":10,"./util/index.js":15}],7:[function(require,module,exports){
 /*!
   * Bootstrap data.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -3023,7 +3023,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"../util/index.js":14}],9:[function(require,module,exports){
+},{"../util/index.js":15}],9:[function(require,module,exports){
 /*!
   * Bootstrap manipulator.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -3203,7 +3203,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"../util/index.js":14}],11:[function(require,module,exports){
+},{"../util/index.js":15}],11:[function(require,module,exports){
 /*!
   * Bootstrap dropdown.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -3607,7 +3607,294 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/manipulator.js":9,"./dom/selector-engine.js":10,"./util/index.js":14,"@popperjs/core":1}],12:[function(require,module,exports){
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/manipulator.js":9,"./dom/selector-engine.js":10,"./util/index.js":15,"@popperjs/core":1}],12:[function(require,module,exports){
+/*!
+  * Bootstrap tab.js v5.3.2 (https://getbootstrap.com/)
+  * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+  */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('./base-component.js'), require('./dom/event-handler.js'), require('./dom/selector-engine.js'), require('./util/index.js')) :
+  typeof define === 'function' && define.amd ? define(['./base-component', './dom/event-handler', './dom/selector-engine', './util/index'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Tab = factory(global.BaseComponent, global.EventHandler, global.SelectorEngine, global.Index));
+})(this, (function (BaseComponent, EventHandler, SelectorEngine, index_js) { 'use strict';
+
+  /**
+   * --------------------------------------------------------------------------
+   * Bootstrap tab.js
+   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+   * --------------------------------------------------------------------------
+   */
+
+
+  /**
+   * Constants
+   */
+
+  const NAME = 'tab';
+  const DATA_KEY = 'bs.tab';
+  const EVENT_KEY = `.${DATA_KEY}`;
+  const EVENT_HIDE = `hide${EVENT_KEY}`;
+  const EVENT_HIDDEN = `hidden${EVENT_KEY}`;
+  const EVENT_SHOW = `show${EVENT_KEY}`;
+  const EVENT_SHOWN = `shown${EVENT_KEY}`;
+  const EVENT_CLICK_DATA_API = `click${EVENT_KEY}`;
+  const EVENT_KEYDOWN = `keydown${EVENT_KEY}`;
+  const EVENT_LOAD_DATA_API = `load${EVENT_KEY}`;
+  const ARROW_LEFT_KEY = 'ArrowLeft';
+  const ARROW_RIGHT_KEY = 'ArrowRight';
+  const ARROW_UP_KEY = 'ArrowUp';
+  const ARROW_DOWN_KEY = 'ArrowDown';
+  const HOME_KEY = 'Home';
+  const END_KEY = 'End';
+  const CLASS_NAME_ACTIVE = 'active';
+  const CLASS_NAME_FADE = 'fade';
+  const CLASS_NAME_SHOW = 'show';
+  const CLASS_DROPDOWN = 'dropdown';
+  const SELECTOR_DROPDOWN_TOGGLE = '.dropdown-toggle';
+  const SELECTOR_DROPDOWN_MENU = '.dropdown-menu';
+  const NOT_SELECTOR_DROPDOWN_TOGGLE = `:not(${SELECTOR_DROPDOWN_TOGGLE})`;
+  const SELECTOR_TAB_PANEL = '.list-group, .nav, [role="tablist"]';
+  const SELECTOR_OUTER = '.nav-item, .list-group-item';
+  const SELECTOR_INNER = `.nav-link${NOT_SELECTOR_DROPDOWN_TOGGLE}, .list-group-item${NOT_SELECTOR_DROPDOWN_TOGGLE}, [role="tab"]${NOT_SELECTOR_DROPDOWN_TOGGLE}`;
+  const SELECTOR_DATA_TOGGLE = '[data-bs-toggle="tab"], [data-bs-toggle="pill"], [data-bs-toggle="list"]'; // TODO: could only be `tab` in v6
+  const SELECTOR_INNER_ELEM = `${SELECTOR_INNER}, ${SELECTOR_DATA_TOGGLE}`;
+  const SELECTOR_DATA_TOGGLE_ACTIVE = `.${CLASS_NAME_ACTIVE}[data-bs-toggle="tab"], .${CLASS_NAME_ACTIVE}[data-bs-toggle="pill"], .${CLASS_NAME_ACTIVE}[data-bs-toggle="list"]`;
+
+  /**
+   * Class definition
+   */
+
+  class Tab extends BaseComponent {
+    constructor(element) {
+      super(element);
+      this._parent = this._element.closest(SELECTOR_TAB_PANEL);
+      if (!this._parent) {
+        return;
+        // TODO: should throw exception in v6
+        // throw new TypeError(`${element.outerHTML} has not a valid parent ${SELECTOR_INNER_ELEM}`)
+      }
+
+      // Set up initial aria attributes
+      this._setInitialAttributes(this._parent, this._getChildren());
+      EventHandler.on(this._element, EVENT_KEYDOWN, event => this._keydown(event));
+    }
+
+    // Getters
+    static get NAME() {
+      return NAME;
+    }
+
+    // Public
+    show() {
+      // Shows this elem and deactivate the active sibling if exists
+      const innerElem = this._element;
+      if (this._elemIsActive(innerElem)) {
+        return;
+      }
+
+      // Search for active tab on same parent to deactivate it
+      const active = this._getActiveElem();
+      const hideEvent = active ? EventHandler.trigger(active, EVENT_HIDE, {
+        relatedTarget: innerElem
+      }) : null;
+      const showEvent = EventHandler.trigger(innerElem, EVENT_SHOW, {
+        relatedTarget: active
+      });
+      if (showEvent.defaultPrevented || hideEvent && hideEvent.defaultPrevented) {
+        return;
+      }
+      this._deactivate(active, innerElem);
+      this._activate(innerElem, active);
+    }
+
+    // Private
+    _activate(element, relatedElem) {
+      if (!element) {
+        return;
+      }
+      element.classList.add(CLASS_NAME_ACTIVE);
+      this._activate(SelectorEngine.getElementFromSelector(element)); // Search and activate/show the proper section
+
+      const complete = () => {
+        if (element.getAttribute('role') !== 'tab') {
+          element.classList.add(CLASS_NAME_SHOW);
+          return;
+        }
+        element.removeAttribute('tabindex');
+        element.setAttribute('aria-selected', true);
+        this._toggleDropDown(element, true);
+        EventHandler.trigger(element, EVENT_SHOWN, {
+          relatedTarget: relatedElem
+        });
+      };
+      this._queueCallback(complete, element, element.classList.contains(CLASS_NAME_FADE));
+    }
+    _deactivate(element, relatedElem) {
+      if (!element) {
+        return;
+      }
+      element.classList.remove(CLASS_NAME_ACTIVE);
+      element.blur();
+      this._deactivate(SelectorEngine.getElementFromSelector(element)); // Search and deactivate the shown section too
+
+      const complete = () => {
+        if (element.getAttribute('role') !== 'tab') {
+          element.classList.remove(CLASS_NAME_SHOW);
+          return;
+        }
+        element.setAttribute('aria-selected', false);
+        element.setAttribute('tabindex', '-1');
+        this._toggleDropDown(element, false);
+        EventHandler.trigger(element, EVENT_HIDDEN, {
+          relatedTarget: relatedElem
+        });
+      };
+      this._queueCallback(complete, element, element.classList.contains(CLASS_NAME_FADE));
+    }
+    _keydown(event) {
+      if (![ARROW_LEFT_KEY, ARROW_RIGHT_KEY, ARROW_UP_KEY, ARROW_DOWN_KEY, HOME_KEY, END_KEY].includes(event.key)) {
+        return;
+      }
+      event.stopPropagation(); // stopPropagation/preventDefault both added to support up/down keys without scrolling the page
+      event.preventDefault();
+      const children = this._getChildren().filter(element => !index_js.isDisabled(element));
+      let nextActiveElement;
+      if ([HOME_KEY, END_KEY].includes(event.key)) {
+        nextActiveElement = children[event.key === HOME_KEY ? 0 : children.length - 1];
+      } else {
+        const isNext = [ARROW_RIGHT_KEY, ARROW_DOWN_KEY].includes(event.key);
+        nextActiveElement = index_js.getNextActiveElement(children, event.target, isNext, true);
+      }
+      if (nextActiveElement) {
+        nextActiveElement.focus({
+          preventScroll: true
+        });
+        Tab.getOrCreateInstance(nextActiveElement).show();
+      }
+    }
+    _getChildren() {
+      // collection of inner elements
+      return SelectorEngine.find(SELECTOR_INNER_ELEM, this._parent);
+    }
+    _getActiveElem() {
+      return this._getChildren().find(child => this._elemIsActive(child)) || null;
+    }
+    _setInitialAttributes(parent, children) {
+      this._setAttributeIfNotExists(parent, 'role', 'tablist');
+      for (const child of children) {
+        this._setInitialAttributesOnChild(child);
+      }
+    }
+    _setInitialAttributesOnChild(child) {
+      child = this._getInnerElement(child);
+      const isActive = this._elemIsActive(child);
+      const outerElem = this._getOuterElement(child);
+      child.setAttribute('aria-selected', isActive);
+      if (outerElem !== child) {
+        this._setAttributeIfNotExists(outerElem, 'role', 'presentation');
+      }
+      if (!isActive) {
+        child.setAttribute('tabindex', '-1');
+      }
+      this._setAttributeIfNotExists(child, 'role', 'tab');
+
+      // set attributes to the related panel too
+      this._setInitialAttributesOnTargetPanel(child);
+    }
+    _setInitialAttributesOnTargetPanel(child) {
+      const target = SelectorEngine.getElementFromSelector(child);
+      if (!target) {
+        return;
+      }
+      this._setAttributeIfNotExists(target, 'role', 'tabpanel');
+      if (child.id) {
+        this._setAttributeIfNotExists(target, 'aria-labelledby', `${child.id}`);
+      }
+    }
+    _toggleDropDown(element, open) {
+      const outerElem = this._getOuterElement(element);
+      if (!outerElem.classList.contains(CLASS_DROPDOWN)) {
+        return;
+      }
+      const toggle = (selector, className) => {
+        const element = SelectorEngine.findOne(selector, outerElem);
+        if (element) {
+          element.classList.toggle(className, open);
+        }
+      };
+      toggle(SELECTOR_DROPDOWN_TOGGLE, CLASS_NAME_ACTIVE);
+      toggle(SELECTOR_DROPDOWN_MENU, CLASS_NAME_SHOW);
+      outerElem.setAttribute('aria-expanded', open);
+    }
+    _setAttributeIfNotExists(element, attribute, value) {
+      if (!element.hasAttribute(attribute)) {
+        element.setAttribute(attribute, value);
+      }
+    }
+    _elemIsActive(elem) {
+      return elem.classList.contains(CLASS_NAME_ACTIVE);
+    }
+
+    // Try to get the inner element (usually the .nav-link)
+    _getInnerElement(elem) {
+      return elem.matches(SELECTOR_INNER_ELEM) ? elem : SelectorEngine.findOne(SELECTOR_INNER_ELEM, elem);
+    }
+
+    // Try to get the outer element (usually the .nav-item)
+    _getOuterElement(elem) {
+      return elem.closest(SELECTOR_OUTER) || elem;
+    }
+
+    // Static
+    static jQueryInterface(config) {
+      return this.each(function () {
+        const data = Tab.getOrCreateInstance(this);
+        if (typeof config !== 'string') {
+          return;
+        }
+        if (data[config] === undefined || config.startsWith('_') || config === 'constructor') {
+          throw new TypeError(`No method named "${config}"`);
+        }
+        data[config]();
+      });
+    }
+  }
+
+  /**
+   * Data API implementation
+   */
+
+  EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DATA_TOGGLE, function (event) {
+    if (['A', 'AREA'].includes(this.tagName)) {
+      event.preventDefault();
+    }
+    if (index_js.isDisabled(this)) {
+      return;
+    }
+    Tab.getOrCreateInstance(this).show();
+  });
+
+  /**
+   * Initialize on focus
+   */
+  EventHandler.on(window, EVENT_LOAD_DATA_API, () => {
+    for (const element of SelectorEngine.find(SELECTOR_DATA_TOGGLE_ACTIVE)) {
+      Tab.getOrCreateInstance(element);
+    }
+  });
+  /**
+   * jQuery
+   */
+
+  index_js.defineJQueryPlugin(Tab);
+
+  return Tab;
+
+}));
+
+
+},{"./base-component.js":3,"./dom/event-handler.js":8,"./dom/selector-engine.js":10,"./util/index.js":15}],13:[function(require,module,exports){
 /*!
   * Bootstrap component-functions.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -3651,7 +3938,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"../dom/event-handler.js":8,"../dom/selector-engine.js":10,"./index.js":14}],13:[function(require,module,exports){
+},{"../dom/event-handler.js":8,"../dom/selector-engine.js":10,"./index.js":15}],14:[function(require,module,exports){
 /*!
   * Bootstrap config.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -3721,7 +4008,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"../dom/manipulator.js":9,"./index.js":14}],14:[function(require,module,exports){
+},{"../dom/manipulator.js":9,"./index.js":15}],15:[function(require,module,exports){
 /*!
   * Bootstrap index.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -4005,7 +4292,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{}],15:[function(require,module,exports){
+},{}],16:[function(require,module,exports){
 /*!
   * Bootstrap swipe.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -4142,7 +4429,7 @@ exports.preventOverflow = preventOverflow$1;
 }));
 
 
-},{"../dom/event-handler.js":8,"./config.js":13,"./index.js":14}],16:[function(require,module,exports){
+},{"../dom/event-handler.js":8,"./config.js":14,"./index.js":15}],17:[function(require,module,exports){
 window.bootstrap = {};
 
 require('@popperjs/core');
@@ -4156,7 +4443,7 @@ require('bootstrap/js/dist/dropdown');
 // require('bootstrap/js/dist/offcanvas');
 // require('bootstrap/js/dist/popover');
 // require('bootstrap/js/dist/scrollspy');
-// require('bootstrap/js/dist/tab');
+require('bootstrap/js/dist/tab');
 // require('bootstrap/js/dist/toast');
 // require('bootstrap/js/dist/tooltip');
-},{"@popperjs/core":1,"bootstrap/js/dist/alert":2,"bootstrap/js/dist/base-component":3,"bootstrap/js/dist/button":4,"bootstrap/js/dist/carousel":5,"bootstrap/js/dist/collapse":6,"bootstrap/js/dist/dropdown":11}]},{},[16]);
+},{"@popperjs/core":1,"bootstrap/js/dist/alert":2,"bootstrap/js/dist/base-component":3,"bootstrap/js/dist/button":4,"bootstrap/js/dist/carousel":5,"bootstrap/js/dist/collapse":6,"bootstrap/js/dist/dropdown":11,"bootstrap/js/dist/tab":12}]},{},[17]);
