f1cdfec6ca2104a4bfc00d2069c88577f6471c23
Jagruk Poudel <jagruk@bitbucket.org>
2023-11-24 14:16:43 +0930

    megamenu - WIP

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -33,6 +33,7 @@ function fadeIn(el, display) {
 }
 
 (function () {
+    initMegaMenu();
     initPriorityNav();
     initResponsiveMenu();
     initMmenu();
@@ -46,6 +47,36 @@ function fadeIn(el, display) {
     initFlickity();
     initCountUp();
 })();
+
+function initMegaMenu() {
+    
+    $("#mainmenu").accessibleMegaMenu({
+        /* prefix for generated unique id attributes, which are required 
+           to indicate aria-owns, aria-controls and aria-labelledby */
+        uuidPrefix: "accessible-megamenu",
+        
+        /* css class used to define the megamenu styling */
+        menuClass: "nav-menu",
+        
+        /* css class for a top-level navigation item in the megamenu */
+        topNavItemClass: "nav-item",
+        
+        /* css class for a megamenu panel */
+        panelClass: "sub-nav",
+        
+        /* css class for a group of items within a megamenu panel */
+        panelGroupClass: "sub-nav-group",
+        
+        /* css class for the hover state */
+        hoverClass: "hover",
+        
+        /* css class for the focus state */
+        focusClass: "focus",
+        
+        /* css class for the open state */
+        openClass: "open"
+    });
+}
 
 function initPriorityNav() {
     var mainNav = document.querySelector('.ntg-main-nav');
