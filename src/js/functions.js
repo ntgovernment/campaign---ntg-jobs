802a2228884e3d7d50a5731f043aa61b1f049d05
Alex <alex@brainiumlabs.com.au>
2023-12-04 02:11:45 +0930

    video control functionality

--- src/js/functions.js
+++ src/js/functions.js
@@ -12,8 +12,9 @@
     // initImageComparison();
     initResponsiveTable();
     initFlickity();
-    initCountUp();
+    // initCountUp();
     initResizeButtons();
+    initVideoControl();
 })();
 
 function initStickyHeader() {
@@ -514,6 +515,44 @@ function initResizeButtons() {
             e.addClass('btn-sm');
         } else {
             e.removeClass('btn-sm');
+        }
+    }
+}
+
+function initVideoControl() {
+    var btns = document.querySelectorAll('.video-control');
+
+    btns.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            handlePlayPause(btn);
+        });
+    });
+
+    function handlePlayPause(e) {
+        var videoID = e.getAttribute('data-video');
+        if (videoID) {
+            var video = document.getElementById(videoID);
+            if (!video) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        
+        if (e.classList.contains('pause')) {
+            // switch icon
+            e.classList.remove('pause');
+            e.classList.add('play');
+
+            // pause video
+            video.pause();
+        } else if (e.classList.contains('play')) {
+            // switch icon
+            e.classList.remove('play');
+            e.classList.add('pause');
+
+            // play video
+            video.play();
         }
     }
 }
