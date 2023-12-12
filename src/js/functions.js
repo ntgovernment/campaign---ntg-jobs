c633d8433ed88fc26d2f50896ae37d772f9d6a6b
Jagruk Poudel <jagruk@bitbucket.org>
2023-12-12 10:20:12 +0930

    footer js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,8 @@
 let mapTabCarousel;
+let currentWindowWidth = $(window).width(), currentWindowHeight = $(window).height();
 
 (function () {
+    
     let jobsMenuInit = false;
 
     initMegaMenu();
@@ -102,129 +104,246 @@ function initSlidingMenu() {
 }
 
 function initTabsAsAccordions() {
-   // Screen-width breakpoint
+    // Screen-width breakpoint
     const tc_breakpoint = 992;
-	
-    const collapseElementList = document.querySelectorAll('.vertab-content .panel-collapse.collapse');
-    const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl, {
-        toggle: false
-    }));
-    
-	// Switch tabs and update panels classes - Adjust container height
-    jQuery(".vertab-container .vertab-menu .list-group a").click(function(e) 
-	{
-        var index = jQuery(this).index();
-		var container = jQuery(this).parents('.vertab-container');
-		var accordion = container.find('.vertab-accordion');
-		var contents = accordion.find(".vertab-content");
-		
-		e.preventDefault();
-		
-        jQuery(this).addClass("active");
-        jQuery(this).siblings('a.active').removeClass("active");
-        
-		contents.removeClass("active");
+
+    const collapseElementList = $('.vertab-content .panel-collapse.collapse');
+    const collapseList = collapseElementList.map((_, collapseEl) => new bootstrap.Collapse(collapseEl, { toggle: false }));
+
+    // Switch tabs and update panels classes - Adjust container height
+    $(".vertab-container .vertab-menu .list-group a").click(function (e) {
+        var index = $(this).index();
+        var container = $(this).parents('.vertab-container');
+        var accordion = container.find('.vertab-accordion');
+        var contents = accordion.find(".vertab-content");
+
+        e.preventDefault();
+
+        $(this).addClass("active");
+        $(this).siblings('a.active').removeClass("active");
+
+        contents.removeClass("active");
         contents.eq(index).addClass("active");
-		container.data('current',index);
+        container.data('current', index);
     });
-	
-	// Collapse accordion panels (except the one the user just opened) and add "active" class to the panel heading 
-	jQuery('.vertab-accordion').on('show.bs.collapse','.collapse', function() 
-	{
-		var accordion, container, current, index;
-		
-		accordion = jQuery(this).parents('.vertab-accordion');
-		container = accordion.parents('.vertab-container');
-		
-        collapseList.map(collapse => collapse._element.classList.contains("show") && collapse.hide());
 
-		jQuery(this).siblings('.panel-heading').addClass('active');
-		
-		current = accordion.find('.panel-heading.active');
-		index = accordion.find('.panel-heading').index(current);
-		
-		container.data('current',index);
-	});
-								   
-	// Remove "active" class from heading when collapsing the current panel 
-	jQuery('.vertab-accordion .panel-collapse').on('hide.bs.collapse', function () {
-		jQuery(this).siblings('.panel-heading').removeClass('active');
-	});	
-	
-	// Manage resize / rotation events
-	jQuery( window ).on( "resize", debounce(function() 
-	{   
-        resize_vertical_accordions();
-	})) ;
-	
-	// Scroll accordion to show the current panel
-	jQuery(".vertab-accordion .panel-heading").click(function () 
-	{
-		var el = this;
-		setTimeout(function(){jQuery("html, body").animate({scrollTop: jQuery(el).offset().top - 10 }, 200);},200);
-		
-		return true;
-	});
-	
-	//Initial Panels setup
-	resize_vertical_accordions(  );
+    // Collapse accordion panels (except the one the user just opened) and add "active" class to the panel heading
+    $('.vertab-accordion').on('show.bs.collapse', '.collapse', function () {
+        var accordion, container, current, index;
 
-    function resize_vertical_accordions(  ) 
-    {
-        "use strict";
+        accordion = $(this).parents('.vertab-accordion');
+        container = accordion.parents('.vertab-container');
 
-        jQuery('.vertab-container').each(function(i, e)
-        {
-            var index, menu, contents; 
-            var container = jQuery(this);
-            
+        collapseList.each((_, collapse) => {
+            if (collapse._element.classList.contains("show")) {
+                collapse.hide();
+            }
+        });
+
+        $(this).siblings('.panel-heading').addClass('active');
+
+        current = accordion.find('.panel-heading.active');
+        index = accordion.find('.panel-heading').index(current);
+
+        container.data('current', index);
+    });
+
+    // Remove "active" class from heading when collapsing the current panel
+    $('.vertab-accordion .panel-collapse').on('hide.bs.collapse', function () {
+        $(this).siblings('.panel-heading').removeClass('active');
+    });
+
+    // Manage resize / rotation events
+    $(window).on("resize", debounce(function () {
+        if($(window).width() != currentWindowWidth || $(window).height() != currentWindowHeight){
+            resizeVerticalAccordions();
+            currentWindowWidth = $(window).width(), currentWindowHeight = $(window).height();
+        }
+    }));
+
+    // Scroll accordion to show the current panel
+    $(".vertab-accordion .panel-heading").click(function () {
+        var el = this;
+        setTimeout(function () {
+            $("html, body").animate({ scrollTop: $(el).offset().top - 10 }, 200);
+        }, 200);
+
+        return true;
+    });
+
+    // Initial Panels setup
+    resizeVerticalAccordions();
+
+    function resizeVerticalAccordions() {
+        $('.vertab-container').each(function () {
+            var index, menu, contents;
+            var container = $(this);
+
             // Setup current tab/panel (default to first tab/panel)
-            index = jQuery(this).data('current');
+            index = container.data('current');
 
-            if(index === undefined)
-            {
-                jQuery(this).data('index',0);
+            if (index === undefined) {
+                container.data('index', 0);
                 index = 0;
             }
-            
+
             // If using a desktop-size screen, manage as tabbed panels
-            if( jQuery( window ).width() > tc_breakpoint)
-            {
-                collapseList.map(collapse => collapse.show());
-                
+            if ($(window).width() > tc_breakpoint) {
+                collapseList.each((_, collapse) => collapse.show());
+
                 // Reset panels heights (Bootstrap's accordions sets heights to zero)
-                jQuery(this).find('.panel-collapse.collapse').css('height','auto');
-                
+                container.find('.panel-collapse.collapse').css('height', 'auto');
+
                 // Clean tab-navigation styles
-                menu = jQuery(this).find('.vertab-menu .list-group a');
+                menu = container.find('.vertab-menu .list-group a');
                 menu.removeClass("active");
 
                 // Clean tab-panels styles
-                contents = jQuery(this).find(".vertab-accordion .vertab-content");
+                contents = container.find(".vertab-accordion .vertab-content");
                 contents.removeClass("active");
-                
+
                 // Update tab navigation and panels styles
-                menu.eq(index).addClass('active');			
+                menu.eq(index).addClass('active');
                 contents.eq(index).addClass("active");
+            } else { 
+                collapseList.each((_, collapse) => collapse.hide());
+
+                container.find('.vertab-content .panel-heading').removeClass('active');
+
+                setTimeout(function () {
+                    container.find('.vertab-content .panel-heading').eq(index).addClass("active");
+                    collapseList[index].show();
+                }, 1000);
             }
-            else // If using a mobile device (phone + tablets), manage as accordion
-            {   
-                collapseList.map(collapse => collapse.hide());
-
-                // Clean styles from headings
-                jQuery(this).find('.vertab-content .panel-heading').removeClass('active');
-                
-                // Wait until all panels have collapsed and mark the one the user selected as active.
-                setTimeout(function()
-                {
-                    jQuery(container).find('.vertab-content .panel-heading').eq(index).addClass("active");
-                    collapseList[index].show();				
-                },1000);
-
-            }	
-        });	
+        });
     }
 }
+
+
+// function initTabsAsAccordions() {
+//    // Screen-width breakpoint
+//     const tc_breakpoint = 992;
+	
+//     const collapseElementList = document.querySelectorAll('.vertab-content .panel-collapse.collapse');
+//     const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl, {
+//         toggle: false
+//     }));
+    
+// 	// Switch tabs and update panels classes - Adjust container height
+//     jQuery(".vertab-container .vertab-menu .list-group a").click(function(e) 
+// 	{
+//         var index = jQuery(this).index();
+// 		var container = jQuery(this).parents('.vertab-container');
+// 		var accordion = container.find('.vertab-accordion');
+// 		var contents = accordion.find(".vertab-content");
+		
+// 		e.preventDefault();
+		
+//         jQuery(this).addClass("active");
+//         jQuery(this).siblings('a.active').removeClass("active");
+        
+// 		contents.removeClass("active");
+//         contents.eq(index).addClass("active");
+// 		container.data('current',index);
+//     });
+	
+// 	// Collapse accordion panels (except the one the user just opened) and add "active" class to the panel heading 
+// 	jQuery('.vertab-accordion').on('show.bs.collapse','.collapse', function() 
+// 	{
+// 		var accordion, container, current, index;
+		
+// 		accordion = jQuery(this).parents('.vertab-accordion');
+// 		container = accordion.parents('.vertab-container');
+		
+//         collapseList.map(collapse => collapse._element.classList.contains("show") && collapse.hide());
+
+// 		jQuery(this).siblings('.panel-heading').addClass('active');
+		
+// 		current = accordion.find('.panel-heading.active');
+// 		index = accordion.find('.panel-heading').index(current);
+		
+// 		container.data('current',index);
+// 	});
+								   
+// 	// Remove "active" class from heading when collapsing the current panel 
+// 	jQuery('.vertab-accordion .panel-collapse').on('hide.bs.collapse', function () {
+// 		jQuery(this).siblings('.panel-heading').removeClass('active');
+// 	});	
+	
+// 	// Manage resize / rotation events
+// 	jQuery( window ).on( "resize", debounce(function() 
+// 	{   
+//         resize_vertical_accordions();
+// 	})) ;
+	
+// 	// Scroll accordion to show the current panel
+// 	jQuery(".vertab-accordion .panel-heading").click(function () 
+// 	{
+// 		var el = this;
+// 		setTimeout(function(){jQuery("html, body").animate({scrollTop: jQuery(el).offset().top - 10 }, 200);},200);
+		
+// 		return true;
+// 	});
+	
+// 	//Initial Panels setup
+// 	resize_vertical_accordions(  );
+
+//     function resize_vertical_accordions(  ) 
+//     {
+//         "use strict";
+
+//         jQuery('.vertab-container').each(function(i, e)
+//         {
+//             var index, menu, contents; 
+//             var container = jQuery(this);
+            
+//             // Setup current tab/panel (default to first tab/panel)
+//             index = jQuery(this).data('current');
+
+//             if(index === undefined)
+//             {
+//                 jQuery(this).data('index',0);
+//                 index = 0;
+//             }
+            
+//             // If using a desktop-size screen, manage as tabbed panels
+//             if( jQuery( window ).width() > tc_breakpoint)
+//             {
+//                 collapseList.map(collapse => collapse.show());
+                
+//                 // Reset panels heights (Bootstrap's accordions sets heights to zero)
+//                 jQuery(this).find('.panel-collapse.collapse').css('height','auto');
+                
+//                 // Clean tab-navigation styles
+//                 menu = jQuery(this).find('.vertab-menu .list-group a');
+//                 menu.removeClass("active");
+
+//                 // Clean tab-panels styles
+//                 contents = jQuery(this).find(".vertab-accordion .vertab-content");
+//                 contents.removeClass("active");
+                
+//                 // Update tab navigation and panels styles
+//                 menu.eq(index).addClass('active');			
+//                 contents.eq(index).addClass("active");
+//             }
+//             else // If using a mobile device (phone + tablets), manage as accordion
+//             {   
+//                 collapseList.map(collapse => collapse.hide());
+
+//                 // Clean styles from headings
+//                 jQuery(this).find('.vertab-content .panel-heading').removeClass('active');
+                
+//                 // Wait until all panels have collapsed and mark the one the user selected as active.
+//                 setTimeout(function()
+//                 {
+//                     jQuery(container).find('.vertab-content .panel-heading').eq(index).addClass("active");
+//                     collapseList[index].show();				
+//                 },1000);
+
+//             }	
+//         });	
+//     }
+// }
 
 function initInteractiveMap() {
     const interactiveMapWrapper = document.getElementById("interactive-map");
