d066067fcd8ef7098478af29b3dc2e6ec60b56d5
Alex <alex@brainiumlabs.com.au>
2024-09-13 11:31:11 +0930

    job sliding menu - set active link as selected cell

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -108,7 +108,20 @@ function initJobsSlidingMenu() {
                     percentPosition: false,
                     pageDots: false,
                     contain: false
-                });    
+                });
+
+                // set active link as selected cell
+                const carousel = document.getElementById('jobCarouselMenu');
+                const cells = carousel.querySelectorAll('.nav-cell');
+                const cellsArr = Array.from(cells);
+                console.log(cellsArr);
+                
+                for (let i = 0; i < cellsArr.length; i++) {
+                    var cell = cellsArr[i];
+                    if (cell.querySelector('a.active')) {
+                        jobCarouselMenu.select(i, false, true);
+                    }
+                }
             }
         }
     }
