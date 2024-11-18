c0b1ee400b64cddc2d797f48c1c7bced801a4b00
Jagruk <jagruk.paudel@gmail.com>
2024-11-18 07:47:09 +0545

    add vacacncy no to job file viewer

--- src/js/plugins/fuse-search.js
+++ src/js/plugins/fuse-search.js
@@ -303,8 +303,6 @@ class NTGJobSearch {
                         ${this._getSalaryDetails(vacancyDesignationList) ? this._getSalaryDetails(vacancyDesignationList).salaryText : ''}
                     </div>
                 </a>
-                
-
             </div>
             <div id="collapse-${rtfId}" class="accordion-collapse multi-collapse accordion-item-content collapse" data-bs-parent="#jobsearchAccordion">
                 <div class="accordion-body"></div>
@@ -322,7 +320,7 @@ class NTGJobSearch {
             specialInstructions && accordionBody.appendChild(this._createDescriptionRow("Special Instructions", specialInstructions));
 
             locationList.length > 0 && accordionBody.appendChild(this._createDescriptionRow("Locations", locationList, "location"));
-            attachmentsList.length > 0 && accordionBody.appendChild(this._createDescriptionRow("Attachments", attachmentsList, "attachments"));
+            attachmentsList.length > 0 && accordionBody.appendChild(this._createDescriptionRow("Attachments", attachmentsList, "attachments", positionNumber));
 
             const jobUrl = `https://jointheterritory.nt.gov.au/vacancy?id=${positionNumber}&banner=1322978`;
             const jobUrlLinkedin = `https://jointheterritory.nt.gov.au/vacancy?id%3D${positionNumber}&banner%3D1322978`;
@@ -550,7 +548,7 @@ class NTGJobSearch {
      * @param {String} specialDesc | location and attachments specialDesc available, will generate the descriptions based on those
      * @returns 
      */
-    _createDescriptionRow(title, description, specialDesc) {
+    _createDescriptionRow(title, description, specialDesc, vacancyNo) {
         let row = document.createElement("div");
         row.classList.add("row","mb-2");
 
@@ -596,7 +594,7 @@ class NTGJobSearch {
                     template = `<tr>
                         <td>${attachment.fileName && attachment.fileName.split("-")[0]}</td>
                         <td>HTML</td>
-                        <td>${attachment.fileExtension == "docx" ? `<a href="${this.searchResultsWrapper.getAttribute("data-url-dochtml")}?id=${attachmentId}" class="d-block text-nowrap view-online" target="_blank" rel="noopener" title="Opens in a new window">View Online<i class="fas fa-eye ms-1"></i></a></td>` : "</td>"}
+                        <td>${attachment.fileExtension == "docx" ? `<a href="${this.searchResultsWrapper.getAttribute("data-url-dochtml")}?attachmentId=${attachmentId}&id=${vacancyNo}" class="d-block text-nowrap view-online" target="_blank" rel="noopener" title="Opens in a new window">View Online<i class="fas fa-eye ms-1"></i></a></td>` : "</td>"}
                     </tr>` + template;
                 }
 
